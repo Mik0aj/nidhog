388253dd4d16259babed39e9c4ed51637dc0cfef
Mik0aj <mikoajmoson@gmail.com>
2024-05-08 21:26:38 +0200

    added some new states

--- src/game_menu_state_machine.py
+++ src/game_menu_state_machine.py
@@ -21,7 +21,29 @@ class State:
             transitions.extend(self.next_state.get_transitions_to_state(state))
         return transitions
 
-
+    # def get_transitions_to_state(self, state, through_state=None):
+    #     transitions = []
+    #     if through_state is not None:
+    #         # If a through_state is specified, we first check if the current state is the through_state
+    #         if self == through_state:
+    #             # If the current state is the through_state, we then check if the next_state is the target state
+    #             if self.next_state == state:
+    #                 transitions.append(self.next_state)
+    #             elif self.next_state is not None:
+    #                 # If the next_state is not the target state, we recursively call get_transitions_to_state on the next_state
+    #                 transitions.extend(self.next_state.get_transitions_to_state(state, through_state))
+    #         else:
+    #             # If the current state is not the through_state, we recursively call get_transitions_to_state on the next_state
+    #             if self.next_state is not None:
+    #                 transitions.extend(self.next_state.get_transitions_to_state(state, through_state))
+    #     else:
+    #         # If no through_state is specified, we proceed as before
+    #         if type(self.next_state) == type(state):
+    #             transitions.append(self.next_state)
+    #         elif self.next_state is not None:
+    #             transitions.append(self.next_state)
+    #             transitions.extend(self.next_state.get_transitions_to_state(state))
+    #     return transitions
 
 class StartScreen(State):
     def __init__(self):
@@ -37,8 +59,6 @@ class StartScreen(State):
     def enter(self):
         return self.next_state
     
-
-
 class MenuState(State):
     def __init__(self, name, menu_items):
         super().__init__(name)
@@ -65,9 +85,39 @@ class MenuState(State):
                 next_state = menu_item.get_transitions_to_state(state)
                 if next_state:
                     transitions.append(menu_item)
-                    transitions.extend(next_state)  # Use extend to add the elements of the list rather than the list itself
+                    transitions.extend(next_state)  
                     return transitions
         return transitions
+    
+    # def get_transitions_to_state(self, state, through_state=None):
+    #     transitions = []
+    #     for menu_item in self.menu_items:
+    #         if through_state is not None:
+    #             if self == through_state:
+    #                 if menu_item == state:
+    #                     transitions.append(menu_item)
+    #                 elif menu_item is not None:
+    #                     next_state = menu_item.get_transitions_to_state(state, through_state)
+    #                     if next_state:
+    #                         transitions.append(menu_item)
+    #                         transitions.extend(next_state)
+    #                         return transitions
+    #             else:
+    #                 if menu_item is not None:
+    #                     next_state = menu_item.get_transitions_to_state(state, through_state)
+    #                     if next_state:
+    #                         transitions.append(menu_item)
+    #                         transitions.extend(next_state)
+    #         else:
+    #             if type(menu_item) == type(state):
+    #                 transitions.append(menu_item)
+    #             elif menu_item is not None:
+    #                 next_state = menu_item.get_transitions_to_state(state)
+    #                 if next_state:
+    #                     transitions.append(menu_item)
+    #                     transitions.extend(next_state)
+    #     return transitions
+    
 class MainMenu(MenuState):
     def __init__(self):
         super().__init__("Main Menu", [
@@ -83,7 +133,17 @@ class MainMenu(MenuState):
 class Multiplayer(MenuState):
     def __init__(self):
         super().__init__("Multiplayer", [
-            OfflineVersus()
+            OfflineVersus(),
+            OnlineVersus(),
+            Variants()
+        ])
+
+class MultiplayerPauseMenu(MenuState):
+    def __init__(self):
+        super().__init__("Multiplayer", [
+            UnpauseGame(),
+            ChangeLevel(),
+            QuitToTitleScreen() #goes to StartingScreen
         ])
 
 class OfflineVersus(State):
@@ -94,31 +154,21 @@ class SinglePlayer(State):
     def __init__(self):
         super().__init__("Single Player")
 
-
-
 class Tournament(State):
     def __init__(self):
         super().__init__("Tournament")
-
-
 
 class Options(State):
     def __init__(self):
         super().__init__("Options")
 
-
-
 class Credits(State):
     def __init__(self):
         super().__init__("Credits")
 
-
-
 class QuitToDesktop(State):
     def __init__(self):
         super().__init__("QuitToDesktop")
-
-
 
 class HowToPlay(State):
     def __init__(self):
@@ -127,4 +177,24 @@ class HowToPlay(State):
 class Begin(State):
     def __init__(self):
         super().__init__("Begin")
+
+class OnlineVersus(State):
+    def __init__(self):
+        super().__init__("OnlineVersus")
+
+class Variants(State):
+    def __init__(self):
+        super().__init__("Variants")
+
+class UnpauseGame(State):
+    def __init__(self):
+        super().__init__("UnpauseGame")
+
+class ChangeLevel(State):
+    def __init__(self):
+        super().__init__("ChangeLevel")
+
+class QuitToTitleScreen(State):
+    def __init__(self):
+        super().__init__("QuitToTitleScreen")
 
